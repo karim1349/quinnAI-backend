425dc3e8328736db75b159511ab372f73983ab11
Karim Benchekroun <karimbenchekroun@outlook.fr>
2023-02-12 22:14:20 +0100

    try to delete problematic setting

--- core/settings.py
+++ core/settings.py
@@ -127,11 +127,6 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# Extra lookup directories for collectstatic to find static files
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
