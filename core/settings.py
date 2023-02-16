2b47e22339bbd27491949b81d207b1aecc0be0f3
Karim Benchekroun <karimbenchekroun@outlook.fr>
2023-02-16 18:18:25 +0100

    Database set to postgres

--- core/settings.py
+++ core/settings.py
@@ -85,8 +85,8 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres://jhowejwzttsoov:5f3611fbe525196bd7c0afaa854e5c283186780e26a1a2d07421f5fdd0526fbd@ec2-54-77-40-202.eu-west-1.compute.amazonaws.com:5432/d7ld50c196f2b2',
     }
 }
 
