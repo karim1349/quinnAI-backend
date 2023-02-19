62d57f17b1cc5be62fb9f8460c8490b0e7b1b986
Karim Benchekroun <karimbenchekroun@outlook.fr>
2023-02-19 16:20:54 +0100

    Lighting the returned data to make the API call faster

--- api/views/Email.py
+++ api/views/Email.py
@@ -14,4 +14,4 @@ class EmailViewSet(ModelViewSet):
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        return Response(serializer.data['body'], status=status.HTTP_201_CREATED, headers=headers)
