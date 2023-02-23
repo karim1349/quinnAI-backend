c79bbf5c3bdf91a217c54faa88ecd62a77c493df
Karim Benchekroun <karimbenchekroun@outlook.fr>
2023-02-23 08:35:48 +0100

    allowed hosts specified

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'django-insecure-3u5qwfvl+1goq_r+8*sylt!v28e&80v4)@3vw%@w_-6p36=$kg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'quinn-development.herokuapp.com',
+    '127.0.0.1'
+]
 
 
 # Application definition
