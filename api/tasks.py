eae43395bdd8bbfb3e6c702338d541eda47f2dc9
gargem <elhassane@fanprime.io>
2023-03-07 07:29:17 +0100

    calling label celery task

--- api/tasks.py
+++ api/tasks.py
@@ -1,12 +1,16 @@
 from pprint import pprint
 
 import googleapiclient
+from django.contrib.auth.models import User
 from googleapiclient.errors import HttpError
 from api.gmail_api import GmailClient
 from api.models import Label
+from core.celery import app
 
 
-def creating_user_labels(user, selected_labels):
+@app.task
+def task_creating_user_labels(user_id, selected_labels):
+    user = User.objects.get(pk=user_id)
     cl = GmailClient(user)
     for label in selected_labels:
         try:
